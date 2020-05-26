2b4e41e85bbf723f1338dd40977211d1fc899da6
Allan Soares <allan.nwrk@gmail.com>
2020-05-26 13:57:35 -0300

    Preventing lxml from self closing empty tags and replacing head tags for p

--- src/python/main.py
+++ src/python/main.py
@@ -2,28 +2,13 @@ from lxml import etree
 from os import listdir
 
 trash = {
-    '<script type="text/javascript" src="../js/teibp.js"/>': '<script type="text/javascript" src="../js/teibp.js"></script>',
-    '<script type="text/javascript" src="../js/main.js"/>': '<script type="text/javascript" src="../js/main.js"></script>',
     '\\n': '',
-    '<style type="text/css" id="tagusage-css"/>': '',
-    '<style type="text/css"/>': '',
-    '<title/>': '',
 }
 
-def remove_head_tags(doc):
-    head_start = doc.index('<head ')
-    head_end = doc[head_start:].index('>') + head_start + 1
-    doc = doc.replace(
-        doc[head_start:head_end], 
-        '<span class="title">'
-    )
-    
-    doc = doc.replace(doc[(doc[head_end:].index('</head>') - 1):(doc[head_end:].index('</head>') + 6)], '</span>')
-    return doc
-
 def treat_string(string, blacklist):
-    for item in blacklist.items():
-        string = string.replace(item[0], item[1])
+    for key in blacklist:
+        if key in string:
+            string = string.replace(key, blacklist[key])
     return string
 
 for filename in listdir('../content/'):
@@ -33,10 +18,20 @@ for filename in listdir('../content/'):
         transform = etree.XSLT(xslt)
         newdom = transform(dom)
 
+        for elem in newdom.iter():
+            tag = elem.tag[elem.tag.index('}') + 1:]
+
+            if tag == 'head':
+                elem.tag = elem.tag.replace(tag, 'p')
+                elem.attrib['class'] = 'title'
+
+            if elem.text == None:
+                elem.text = ' '
+            
+        output = str(etree.tostring(newdom, pretty_print=True), 'utf-8')
         filename = treat_string(filename, {' ': '_', '.xml': '.html'})
         with open('../html/' + filename, 'w') as f:
-            output = str(etree.tostring(newdom, pretty_print=True), 'utf-8')
-            f.write(remove_head_tags(treat_string(output, trash)))
+            f.write(treat_string(output, trash))
     
     except Exception as exc:
         print(filename + ' failed')
